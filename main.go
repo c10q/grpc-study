c0e59ff88c5e6cb0b84556a5cca33c0531d7893e
kwanok <cloq@kakao.com>
2022-01-04 14:30:30 +0900

    grpc 핸들링

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+
+	mailpb "grpc-study/email"
 )
 
 const portNum = "50001"
+
+type mailServer struct {
+	mailpb.MailServer
+}
+
+func (s *mailServer) Send(ctx context.Context, req *mailpb.SendRequest) (*mailpb.SendReply, error) {
+	log.Println(req.To)
+	log.Println(req.From)
+	log.Println(req.Content)
+
+	return &mailpb.SendReply{
+		Success: true,
+	}, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":"+portNum)
@@ -15,6 +32,7 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	mailpb.RegisterMailServer(grpcServer, &mailServer{})
 
 	log.Printf("gRPC server on %s", portNum)
 	if err := grpcServer.Serve(lis); err != nil {
